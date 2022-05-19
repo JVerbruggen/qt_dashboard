207b4ffd3b4fcd836ffa900a3b58f2098e515583
Jurjen Verbruggen <jurjenverbruggen@gmail.com>
2022-05-19 12:28:05 +0200

    edited interface

--- utils/can_reader.py
+++ utils/can_reader.py
@@ -1,6 +1,6 @@
 import can
 
-general_bus = can.Bus(interface='serial', channel='/dev/bus/usb/001/001')
+general_bus = can.Bus(interface='socketcan', channel='/dev/bus/usb/001/001')
 
 while True:
     for msg in bus:
