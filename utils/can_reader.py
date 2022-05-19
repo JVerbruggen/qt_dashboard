54fd5dcd0d51536179f666104de5e6c23e26ec9f
Jurjen Verbruggen <jurjenverbruggen@gmail.com>
2022-05-19 12:47:50 +0200

    added som

--- utils/can_reader.py
+++ utils/can_reader.py
@@ -1,7 +1,9 @@
 import can
 
-general_bus = can.Bus(interface='socketcan', channel='/dev/bus/usb/001/001')
+bus = can.Bus(interface='serial', channel='/dev/ttyUSB0')
 
 while True:
+    print("loop")
     for msg in bus:
+        print("for")
         print(msg.data)
