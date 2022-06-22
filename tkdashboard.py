e5598a49865123fef8b519b656f2f50024498025
Jurjen Verbruggen <jurjenverbruggen@gmail.com>
2022-06-22 12:22:53 +0200

    adding tk support

--- tkdashboard.py
+++ tkdashboard.py
@@ -1,7 +1,59 @@
-import tkinter as tk
+#!/usr/bin/env python3
 
-window = tk.Tk()
-greeting = tk.Label(text="Dit is tk test",width=200, height=200)
-greeting.pack()
+"""
+ZetCode Tkinter tutorial
 
-window.mainloop()
+The example draws lines on the Canvas.
+
+Author: Jan Bodnar
+Website: www.zetcode.com
+"""
+
+from tkinter import Tk, Canvas, Frame, BOTH
+from utils.painter.tkpainter import TkPainter
+from configuration.dashboard_config import DashboardConfig
+from configuration.setup.demo_setup import DemoSetup
+from configuration.setup.serial_setup import SerialSetup
+from configuration.setup.setup import Setup
+from utils.context.qt_context import HMIQtContext
+
+WINDOW = (1600, 900)
+FPS = 60
+
+class TkFrame(Frame):
+    def __init__(self, configuration: DashboardConfig):
+        super().__init__()
+        self.configuration = configuration
+
+        self.initUI()
+
+
+    def draw_loop(self):
+        for drawable in self.configuration.get_drawables():
+            drawable.draw(painter)
+
+    def initUI(self):
+
+        self.master.title("Lines")
+        self.pack(fill=BOTH, expand=1)
+
+        canvas = Canvas(self)
+        painter = TkPainter(canvas)
+
+
+        canvas.pack(fill=BOTH, expand=1)
+
+
+def main():
+    setup = DemoSetup()
+    context = HMIQtContext()
+    configuration = setup.create(context, WINDOW)
+
+    root = Tk()
+    ex = TkFrame(configuration)
+    root.geometry(f"{WINDOW[0]}x{WINDOW[1]}+10+10")
+    root.mainloop()
+
+
+if __name__ == '__main__':
+    main()
---
+++ utils/painter/tkpainter.py
@@ -0,0 +1,53 @@
+from tkinter import Canvas
+
+
+class TkPainter:
+    """
+    TK Painter
+    """
+    DEFAULT_LINE_WIDTH = 2
+    DEFAULT_ROUNDED_WIDTH = 7
+    DEFAULT_FONT = "TimesMD"
+    FONT_SM = "TimesSM"
+    FONT_SM_BOLD = "TimesSMBold"
+
+    def __init__(self, canvas: Canvas):
+        self.canvas = canvas
+
+
+    def draw_rounded_line(self, from_x: int, from_y: int, to_x: int, to_y: int, width: int = None):
+        """
+        Draws a line with rounded corners.
+        """
+        self.canvas.create_line(from_x, from_y, to_x, to_y, width=width)
+    
+    def draw_arc(self, cx: int, cy: int, rad: int, start_deg: int, length_deg: int, width: int = None):
+        """
+        Draws a part circle.
+        """
+    
+    def draw_box(self, x: int, y: int, w: int, h: int, color: (int,int,int,int)):
+        """
+        Draws a rounded box.
+        """
+    
+    def draw_box_filled(self, x: int, y: int, w: int, h: int, color: (int,int,int,int), width: int=2):
+        """
+        Draws a filled rounded box.
+        """
+
+    def draw_text_at(self, x: int, y: int, w: int, h: int, color: (int,int,int,int), text: str, font_str: str="TimesMD"):
+        """
+        Draw text at given position and size.
+        """
+    
+    def draw_svg(self, img, x: int, y: int, w: int, h: int, color: (int,int,int,int)):
+        """
+        Fill SVG image with a given color.
+        Untyped because image is of unknown type.
+        """
+
+    def get_image_from(self, img_src: str):
+        """
+        Opens an image from source.
+        """
